4c149809da81255e7790456c1b83bf7c6321dce9
Aleksey Koloskov <vsyscoder@gmail.com>
2020-06-20 03:43:34 +0300

    Add HW8-bash/files/parser-last.sh

--- HW8-bash/README.md
+++ HW8-bash/README.md
@@ -20,7 +20,7 @@ Write bash script for cron which send email once a hour. Emails must contain:
 
 There must be a period in email. There must be possible to run only one instance at a time.
 
-## Implementation
+## Implementation. Variant 1
 
 [parser100.sh](./files/parser100.sh)
 
@@ -28,7 +28,7 @@ The script parses nginx's access.log for 100 lines at a time and send statistics
 
 Usage:
 ```shell
-./script.sh -h
+./parser100.sh -h
 Usage: ./script.sh options
         -r|--recipient <destination email> - email address to send statistics to. Default root
         -l|--logfile <path/to/access.log> - path to nginx access.log to analyse. Default ./access.log
@@ -37,7 +37,7 @@ Usage: ./script.sh options
         -h|--help - Print this help and exit
 ```
 
-## How to run
+### How to run
 
 Run vagrant instance
 ```shell
@@ -48,10 +48,49 @@ vagrant ssh
 Go to script's directory and run with or w/o argumants (in this example the script shows top 5 instead of top 10 ips/uris/etc... and sends email to local user `vagrant`)
 ```shell
 cd /vagrant/files
-./script.sh -T 5 -r vagrant
+./parser100.sh -T 5 -r vagrant
 ```
 
 And check user's mailbox
 ```shell
 mail
 ```
+
+## Implementation. Variant 2
+
+[parser-last.sh](./files/parser-last.sh)
+
+Second variant. The script reads lines which were added from last run (or from beginning of file).
+
+Important: by default it saves state to `/var/spool/nginx_log_analyzer` directory. So this path must exists and must be writeable for user who launches the script.
+
+```shell
+Usage: ./parser-last.sh options
+        -r|--recipient <destination email> - email address to send statistics to. Default: 'root'
+        -l|--logfile <path/to/access.log> - path to nginx access.log to analyse. Default: './access.log'
+        -L|--lockfile <path/to/lockfile.lock> - path to lockfile. Default: '/tmp/script.lock'
+        -S|--state-dir <path/to/state/directory> - path to directory to save state. Default: '/var/spool/nginx_log_analyzer'
+        -T|--top <num> - report top <num> results. Default: 10
+        -h|--help - Print this help and exit
+```
+
+### How to run
+
+Run vagrant instance
+```shell
+vagrant up
+vagrant ssh
+```
+
+Go to script's directory and run with or w/o argumants (in this example the script shows top 5 instead of top 10 ips/uris/etc... and sends email to local user `vagrant`)
+```shell
+cd /vagrant/files
+./parser-last.sh -T 5 -r vagrant
+```
+
+And check user's mailbox
+```shell
+mail
+```
+
+It's possible to set from which line start parsing. For this you need to set `FROM_LINE=` variable value in state file (default `/var/spool/nginx_log_analyzer/state.env`)
---
+++ HW8-bash/files/parser-last.sh
@@ -0,0 +1,190 @@
+#!/usr/bin/env bash
+###
+# analyze nginx's access.log and send results to email
+###
+
+set -eo pipefail
+
+# Set default values for variables
+RECIPIENT='root'
+LOGFILE='./access.log'
+LOCKFILE='/tmp/script.lock'
+STATE_DIR='/var/spool/nginx_log_analyzer'
+TOP=10
+
+# Set permanent ariables values
+STATE_FILE_NAME="state.env"
+MSGBODY_FILE=$(mktemp)
+
+# Print usage information
+usage() {
+  echo "Usage: $0 options"
+  printf "\t-r|--recipient <destination email> - email address to send statistics to. Default: '%s'\n" "$RECIPIENT"
+  printf "\t-l|--logfile <path/to/access.log> - path to nginx access.log to analyse. Default: '%s'\n" "$LOGFILE"
+  printf "\t-L|--lockfile <path/to/lockfile.lock> - path to lockfile. Default: '%s'\n" "$LOCKFILE"
+  printf "\t-S|--state-dir <path/to/state/directory> - path to directory to save state. Default: '%s'\n" "$STATE_DIR"
+  printf "\t-T|--top <num> - report top <num> results. Default: %i\n" "$TOP"
+  printf "\t-h|--help - Print this help and exit\n"
+  exit ${1:-0}
+}
+
+# Run on script termination
+terminate() {
+  echo "Terminated! Removing lockfile $LOCKFILE"
+  rm -f "$LOCKFILE"
+  rm -f "$MSGBODY_FILE"
+  exit $?
+}
+
+# Send statistics to email
+send_email() {
+  cat $MSGBODY_FILE | mail -s 'access.log stats' $RECIPIENT
+}
+
+# Parse arguments
+while true; do
+  case "$1" in
+  -r | --recipient)
+    RECIPIENT="${2}"
+    shift 2
+    ;;
+  -l | --logfile)
+    LOGFILE="${2}"
+    shift 2
+    ;;
+  -L | --lockfile)
+    LOCKFILE="${2}"
+    shift 2
+    ;;
+  -S | --state-dir)
+    STATE_DIR="${2}"
+    shift 2
+    ;;
+  -T | --top)
+    TOP="${2}"
+    shift 2
+    ;;
+  -h | --help)
+    usage
+    shift 1
+    ;;
+  --)
+    shift
+    break
+    ;;
+  *) break ;;
+  esac
+done
+
+# debug options
+cat <<EOF
+RECIPIENT=$RECIPIENT
+LOGFILE=$LOGFILE
+LOCKFILE=$LOCKFILE
+STATE_DIR=$STATE_DIR
+TOP=$TOP
+MSGBODY_FILE=$MSGBODY_FILE
+EOF
+
+# Try to acquire lockfile
+if (
+  set -o noclobber
+  echo "$$" >"$LOCKFILE"
+) 2>/dev/null; then
+  trap 'terminate' INT TERM EXIT
+else
+  echo "Failed to acquire lockfile: $LOCKFILE."
+  echo "Held by $(cat ${LOCKFILE})"
+  exit 1
+fi
+
+###############################################
+
+##
+# Define filters
+##
+http_only() {
+  grep --color=never -P ' HTTP/[\d\.]+"'
+}
+# Extract datetime from logline
+log2logdt() {
+  awk '{ print $4" "$5; }' | tr -d "[" | tr -d "]"
+}
+# Convert datetime from access.log format to parseable by 'date --date' argument
+logdt2dt() {
+  tr "/" ":" | tr " " ":" | awk -F":" '{ print $1" "$2" "$3" "$4":"$5":"$6" "$7 ; }'
+}
+# Convert daterime to timestamp
+logdt2ts() {
+  # $1 - date modifier, default: empty
+  xargs -I{} date --date="{} $1" +"%s"
+}
+# Convert timestamp to nginx's log datetime format
+ts2logdt() {
+  # $1 - date modifier, default: empty
+  xargs -I{} date --date="@{} $1" +"%d/%b/%Y:%H:%M:%S %z"
+}
+
+##
+# Define analyzers
+##
+top_n() {
+  awk '{ print $1; }' | sort -n | uniq -c | sort -rn | head -n $TOP
+}
+
+##
+# Define helpers
+##
+# Get last logfile position or create new state file
+get_last_position() {
+  mkdir -p "${STATE_DIR}" || {
+    echo "ERROR: state directory '$STATE_DIR' must exists"
+    usage 1
+  }
+  test -w "${STATE_DIR}" || {
+    echo "ERROR: Directory '$STATE_DIR' must be writable"
+    usage 1
+  }
+  STATE_FILE="${STATE_DIR}/${STATE_FILE_NAME}"
+  test -f "$STATE_FILE" || { echo "FROM_LINE=1" >$STATE_FILE; }
+  source $STATE_FILE
+}
+
+##
+# Read log from $FROM_LINE to end of file
+##
+# Read current state ($FROM_LINE)
+get_last_position
+# Set TO_LINE as file length
+TO_LINE=$(wc -l $LOGFILE | cut -d" " -f1)
+# Check new lines were added
+test $FROM_LINE -eq $TO_LINE && {
+  echo "No new lines. Exiting"
+  exit 0
+}
+# Save last position to $STATE_FILE
+echo "FROM_LINE=$TO_LINE" >$STATE_FILE
+
+# Period
+FROM_DT=$(sed -n "$(($FROM_LINE + 1)),$(($FROM_LINE + 1))p" $LOGFILE | cut -d"[" -f2 | cut -d"]" -f1)
+TO_DT=$(sed -n "$(($TO_LINE)),$(($TO_LINE))p" $LOGFILE | cut -d"[" -f2 | cut -d"]" -f1)
+echo "Period from $FROM_DT to $TO_DT" >$MSGBODY_FILE
+# Top IPs
+echo "--------" >>$MSGBODY_FILE
+echo "Top $TOP IPs" >>$MSGBODY_FILE
+sed -n "$(($FROM_LINE + 1)),$(($TO_LINE))p" $LOGFILE | awk '{ print $1; }' | top_n >>$MSGBODY_FILE
+# Top requested URIs
+echo "--------" >>$MSGBODY_FILE
+echo "Top $TOP requested URIs" >>$MSGBODY_FILE
+sed -n "$(($FROM_LINE + 1)),$(($TO_LINE))p" $LOGFILE | http_only | awk '{ print $7; }' | top_n >>$MSGBODY_FILE
+# Return codes
+echo "--------" >>$MSGBODY_FILE
+echo "Return codes count" >>$MSGBODY_FILE
+sed -n "$(($FROM_LINE + 1)),$(($TO_LINE))p" $LOGFILE | http_only | awk '{ print $9; }' | sort -n | uniq -c | sort -rn >>$MSGBODY_FILE
+# Errors
+echo "--------" >>$MSGBODY_FILE
+printf "Errors (4xx and 5xx):\n\n" >>$MSGBODY_FILE
+sed -n "$(($FROM_LINE + 1)),$(($TO_LINE))p" $LOGFILE | http_only | awk '$9 ~ /[45][0-9]{2}/ { print $0; }' >>$MSGBODY_FILE
+echo "" >>$MSGBODY_FILE
+
+send_email $MSGBODY_FILE
